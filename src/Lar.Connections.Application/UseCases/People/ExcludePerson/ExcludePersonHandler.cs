ef42b542f90b43ce0665b8324a2a77544d7d6635
diego.lobo <diegolclobo@hotmail.com>
2025-05-19 19:03:34 -0300

    feat: adicionar comando e manipulador para excluir pessoa
    Implementa o comando `ExcludePersonCommand` para gerenciar a exclusão de uma pessoa;
    Cria o manipulador `ExcludePersonHandler` que processa o comando e retorna um resultado;
    Define o tipo de resultado `ExcludePersonResult` com a propriedade `Deleted` para indicar sucesso na exclusão.

---
+++ src/Lar.Connections.Application/UseCases/People/ExcludePerson/ExcludePersonHandler.cs
@@ -0,0 +1,12 @@
+﻿using Lar.Connections.Domain.Common;
+using MediatR;
+
+namespace Lar.Connections.Application.UseCases.People.ExcludePerson;
+
+public class ExcludePersonHandler : IRequestHandler<ExcludePersonCommand, Result<ExcludePersonResult>>
+{
+	public Task<Result<ExcludePersonResult>> Handle(ExcludePersonCommand request, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ src/Lar.Connections.Application/UseCases/People/ExcludePerson/ExcludePersonResult.cs
@@ -0,0 +1,3 @@
+﻿namespace Lar.Connections.Application.UseCases.People.ExcludePerson;
+
+public record ExcludePersonResult(bool Deleted);
